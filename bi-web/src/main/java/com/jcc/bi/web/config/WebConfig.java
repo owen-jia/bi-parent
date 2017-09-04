4872171fdca036292a1a1f511429f3ad1cbd8a81
owen jia <owen-jia@outlook.com>
2017-09-04 18:49:32 +0800

    增加基本配置

--- bi-web/src/main/java/com/jcc/bi/web/config/WebConfig.java
+++ bi-web/src/main/java/com/jcc/bi/web/config/WebConfig.java
@@ -1,18 +1,17 @@
 package com.jcc.bi.web.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
+/**
+ * @author Owen Jia
+ */
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     /**
      * 资源文件服务
-     * @author Owen Jia
      * @param registry
      */
     @Override
@@ -24,5 +23,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
         super.addCorsMappings(registry);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor()).addPathPatterns("/**");
+        super.addInterceptors(registry);
     }
 }
