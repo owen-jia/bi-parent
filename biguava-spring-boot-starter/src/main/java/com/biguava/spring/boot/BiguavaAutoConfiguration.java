189022719183fd4db8fc84fcd3eef33b84b7cffb
贾文军 <jiawenjun@isesol.com>
2018-10-23 18:36:04 +0800

    biguava-spring-boot-starter 自动配置sample完成； 修改版本spring-boot-2.0.6

--- biguava-spring-boot-starter/src/main/java/com/biguava/spring/boot/BiguavaAutoConfiguration.java
+++ biguava-spring-boot-starter/src/main/java/com/biguava/spring/boot/BiguavaAutoConfiguration.java
@@ -1,8 +1,9 @@
 package com.biguava.spring.boot;
 
+import com.biguava.spring.boot.annotation.EnableBiguavaConfiguration;
 import com.biguava.spring.boot.service.impl.SayHelloService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
