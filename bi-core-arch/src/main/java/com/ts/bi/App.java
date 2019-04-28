74f9aea781b21e42ac48cf53102af9e183e3b2a3
贾文军 <jiawenjun@isesol.com>
2019-04-28 15:44:07 +0800

    添加文档设计

---
+++ bi-core-arch/src/main/java/com/ts/bi/App.java
@@ -0,0 +1,12 @@
+package com.ts.bi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
